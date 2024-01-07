f8994ff0b7acd1336ffa0bc6fc4051f6585bf238
harshadsupe <65599213+harshadsupe@users.noreply.github.com>
2024-01-07 17:35:03 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+import plotly.express as px
+from folium import plugins
+from folium.plugins import HeatMap
+import warnings
+warnings.filterwarnings('ignore')
+from wordcloud import WordCloud, STOPWORDS
